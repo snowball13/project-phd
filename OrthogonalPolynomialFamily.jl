e57dd2d13b671d68094ae1062b5a5bb88d89d05e
Sheehan Olver <solver@mac.com>
2018-09-14 11:37:09 +0100

    sketch out lazy lanczos

--- OrthogonalPolynomialFamily.jl
+++ OrthogonalPolynomialFamily.jl
@@ -1,13 +1,57 @@
 using ApproxFun
-    import ApproxFun: evaluate, PolynomialSpace, recα, recβ, recγ, recA, recB, recC, domain
+    import ApproxFun: evaluate, PolynomialSpace, recα, recβ, recγ, recA, recB, recC, domain,
+                        domainspace, rangespace, bandinds
+    import Base: getindex
+
+# this finds the OPs and recurrence for a
+function lanczos!(w, P, β, γ, N₀=0)
+
+    N = length(β)
+    x = Fun(identity,space(w))
+
+
+    if N₀ == 0
+        f1=Fun(1/sqrt(sum(w)),space(x))
+
+        P[1] = f1
+
+        v = x*P[1]
+        β[1] = sum(w*v*P[1])
+        v = v - β[1]*P[1]
+        γ[1] = sqrt(sum(w*v^2))
+        P[2] = v/γ[1]
+        N₀ = 1
+    end
+
+    for k = N₀+1:N
+        v = x*P[k] - γ[k-1]*P[k-1]
+        β[k] = sum(w*v*P[k])
+        v = v - β[k]*P[k]
+        γ[k] = sqrt(sum(w*v^2))
+        P[k+1] = v/γ[k]
+    end
+
+    P,β,γ
+end
+
+function lanczos(w, N)
+    P = Array{Fun}(undef, N + 1)
+    β = Array{eltype(w)}(undef, N)
+    γ = Array{eltype(w)}(undef, N)
+    lanczos!(w, P, β, γ)
+end
+
 
 abstract type SpaceFamily{D,R} end
 
-
 struct OrthogonalPolynomialSpace{WW,D,R} <: PolynomialSpace{D,R}
+    α::Tuple{Float64,Float64}
     weight::WW
     a::Vector{R}  # Diagonal recurrence coefficients
     b::Vector{R}  # Off diagonal recurrence coefficients
+    P::Vector{Fun}
+
+    family::OrthogonalPolynomialFamily
 end
 
 domain(S::OrthogonalPolynomialSpace) =
@@ -20,7 +64,7 @@ function resizedata!(S::OrthogonalPolynomialSpace, n)
     n ≤ length(S.a) && return S
     resize!(S.a, n)
     resize!(S.b, n)
-    _, S.a[:], S.b[:] = lanczos(S.weight, n)
+    _, S.a[:], S.b[:] = lanczos(S.weight, n) # would want to reuse computation
     return S
 end
 
@@ -40,7 +84,7 @@ end
 
 function (P::OrthogonalPolynomialFamily{<:Any,<:Any,<:Any,R,N})(α::Vararg{R,N}) where {R,N}
     haskey(P.spaces,α) && return P.spaces[α]
-    P.spaces[α] = OrthogonalPolynomialSpace(prod(P.factors.^α))
+    P.spaces[α] = OrthogonalPolynomialSpace(α, prod(P.factors.^α))
 end
 
 #####
@@ -71,15 +115,32 @@ recC(::Type{T}, S::OrthogonalPolynomialSpace, n) where T =
 
 ## Tests
 
-using Base.Test
+using Test
 
 X = Fun(identity, -1..1)
 P = OrthogonalPolynomialFamily(1+X, 1-X)
 a, b = 0.4, 0.2
-P(a, b)
+Fun(P(a, b), [1.0])(0.1)
 
 
 
+struct OrthogonalPolynomialDerivative{T} <: Derivative{T}
+    space::OrthogonalPolynomialSpace
+end
+
+domainspace(D::OrthogonalPolynomialDerivative) = D.space
+rangespace(D::OrthogonalPolynomialDerivative) = D.space.family(D.space.α .+ 1)
+bandwidths(D::OrthogonalPolynomialDerivative) = (1,?)
+getindex(D::OrthogonalPolynomialDerivative, k::Int, j::Int) = ?
+
+Derivative(sp::OrthogonalPolynomialSpace, k) = (@assert k==1; OrthogonalPolynomialDerivative(sp))
+
+
+
+D = Derivative(Jacobi(a,b))
+@which D[2,3]
+
+bandwidths(D)
 
 ## Jacobi example
 x = Fun()
@@ -87,13 +148,25 @@ P = OrthogonalPolynomialFamily(1+x,1-x)
 a, b = 0.4, 0.2
 @test P(a,b).weight(0.1) ≈ (1+0.1)^a * (1-0.1)^b
 
-P₅ = Fun(Jacobi(a,b), [zeros(5); 1])
-P₅ = P₅/sqrt(sum((1+x)^a*(1-x)^b*P₅^2))
-P̃₅ = Fun(P(a,b), [zeros(5); 1])
+P₅ = Fun(Jacobi(a,b), [zeros(0); 1])
+P₅ = P₅ * sqrt(sum((1+x)^a*(1-x)^b))/sqrt(sum((1+x)^a*(1-x)^b*P₅^2))
+P̃₅ = Fun(P(a,b), [zeros(0); 1])
+
+
+sp = P(a,b)
+
+
 
 # doesn't yet work
 @test P̃₅(0.1) ≈ P₅(0.1)
 
+for n = 1:5
+    P₅ = Fun(Jacobi(a,b), [zeros(n); 1])
+    P₅ = P₅ * sqrt(sum((1+x)^a*(1-x)^b))/sqrt(sum((1+x)^a*(1-x)^b*P₅^2))
+    P̃₅ = Fun(P(a,b), [zeros(n); 1])
+
+    @test P̃₅(0.1) ≈ P₅(0.1)
+end
 
 f = Fun(Jacobi(0.1,0.2), [1.,2.,3.])
 
@@ -106,3 +179,10 @@ recα(Float64, Chebyshev(), 1)
 recβ(Float64, Chebyshev(), 1)
 recγ(Float64, Chebyshev(), 2)
 @which recβ(Float64, Chebyshev(), 2)s
+
+
+@which lanczos(P(a,b).weight, 6)
+
+
+x = Fun(0..1)
+H = OrthogonalPolynomialFamily(x, 1-x^2)
